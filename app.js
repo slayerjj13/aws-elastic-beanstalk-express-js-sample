a55c9fa4c1ea9fa3dfcd9f0cb084edbdf1dda953
Liang ZHANG <zhangliang13@gmail.com>
2024-07-02 16:02:24 +0800

    change for lab5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World – Here I am!'));
+app.get('/', (req, res) => res.send('Hello World – Here I am!' this is Lab5!!!!!!!));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
